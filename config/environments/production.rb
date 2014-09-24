97a0a5274c5efea26463614d55e069d9163c27fc
daram79 <mayin1q84@gmail.com>
2014-09-25 02:48:56 +0900

    edit for css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
   #config.servre_static_assets = false
 
   # Generate digests for assets URLs.
-  config.assets.digest = false
+  config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
