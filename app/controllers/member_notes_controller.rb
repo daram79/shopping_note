433cb580ecfd6926836de3e358536686e97a6f18
daram79 <mayin1q84@gmail.com>
2015-07-23 02:45:43 +0900

    몇시간전 버그 수정

--- app/controllers/member_notes_controller.rb
+++ app/controllers/member_notes_controller.rb
@@ -9,6 +9,7 @@ class MemberNotesController < ApplicationController
     # @member_notes = MemberNote.all
     user_id = params[:id]
     @member_notes = Feed.where(user_id: user_id).order("updated_at desc")
+    @time_word = Hash.new
     @member_notes.each do |member_note|
       @time_word[member_note.id] = time_ago_in_words(member_note.created_at)
     end
@@ -27,6 +28,7 @@ class MemberNotesController < ApplicationController
    user_id = params[:id]
    like_ids = Like.where(user_id: user_id).pluck(:feed_id)
    @member_notes = Feed.where(id: like_ids).order("updated_at desc")
+   @time_word = Hash.new
    @member_notes.each do |member_note|
       @time_word[member_note.id] = time_ago_in_words(member_note.created_at)
     end
